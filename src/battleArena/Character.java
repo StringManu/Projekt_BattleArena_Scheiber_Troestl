db8b561e6d6f58df210899b0d509d24669ab5bd8
StringManu <manuel.scheiber@hak-reutte.ac.at>
2024-02-20 12:20:20 +0100

    variables, Getter and Setter, Constructor

--- src/battleArena/Character.java
+++ src/battleArena/Character.java
@@ -1,5 +1,37 @@
 package battleArena;
 
 public class Character {
+	
+	private String name;
+	private int lifePoints;
+	private boolean specialAbility;
+	
+	public Character(String name, int lifePoints, boolean specialAbility) {
+		super();
+		this.name = name;
+		this.lifePoints = lifePoints;
+		this.specialAbility = specialAbility;
+	}
 
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLifePoints() {
+		return lifePoints;
+	}
+	public void setLifePoints(int lifePoints) {
+		this.lifePoints = lifePoints;
+	}
+	public boolean isSpecialAbility() {
+		return specialAbility;
+	}
+	public void setSpecialAbility(boolean specialAbility) {
+		this.specialAbility = specialAbility;
+	}
+	
+	
+	
 }
