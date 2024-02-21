b3c0ad0eeff6aeafe9d6132d433ac2592fa9ddbb
LucaTröstl <lucsky.cr7@gmail.com>
2024-02-21 09:13:40 +0100

    Added a player1 and a player2 and switch cases (Still subject to change)

--- src/battleArena/BattleArena.java
+++ src/battleArena/BattleArena.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class BattleArena {
 	
-	public void beginFighting() {
+	public void FightingP1(Character player1, Character player2) {
 	Scanner sc = new Scanner(System.in);
 	String move = sc.nextLine();
 	
@@ -12,16 +12,53 @@ public class BattleArena {
 	
 	case "attack":
 		System.out.println("Character is attacking!");
+		player1.attack(player2);
 	break;
 	
 	case "activate special ability":
 		System.out.println("Character is attacking!");
+		player1.setSpecialAbility(true);
 	break;
 	
 	case "deactivate special ability":
 		System.out.println("Character is attacking!");
+		player1.setSpecialAbility(false);
 	break;
+	
+	default:
+		System.out.println("Error!");
+	break;
+	 }
+	sc.close();
 	}
-	}
+	
+	
+	public void FightingP2(Character player1, Character player2) {
+		Scanner sc = new Scanner(System.in);
+		String move = sc.nextLine();
+		
+		switch(move) {
+		
+		case "attack":
+			System.out.println("Character is attacking!");
+			player2.attack(player1);
+		break;
+		
+		case "activate special ability":
+			System.out.println("Character is attacking!");
+			player2.setSpecialAbility(true);
+		break;
+		
+		case "deactivate special ability":
+			System.out.println("Character is attacking!");
+			player2.setSpecialAbility(false);
+		break;
+		
+		default:
+			System.out.println("Error!");
+		break;
+		 }
+		sc.close();
+		}
 	
 }
