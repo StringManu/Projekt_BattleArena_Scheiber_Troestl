1365530c7a7aa979e1bb6e8282395243cd44c875
LucaTröstl <lucsky.cr7@gmail.com>
2024-02-21 08:58:17 +0100

    Created a prototype of battle arena. (Subject to change!)

--- src/battleArena/BattleArena.java
+++ src/battleArena/BattleArena.java
@@ -1,5 +1,27 @@
 package battleArena;
 
+import java.util.Scanner;
+
 public class BattleArena {
+	
+	Scanner sc = new Scanner(System.in);
+	String move = sc.nextLine();
+	
+	switch(sc) {
+	
+	case "attack":
+		System.out.println("Character is attacking!");
+	break;
+	
+	case "activate special ability":
+		System.out.println("Character is attacking!");
+	break;
+	
+	case "deactivate special ability":
+		System.out.println("Character is attacking!");
+	break;
+	
+	
+	}
 	
 }
